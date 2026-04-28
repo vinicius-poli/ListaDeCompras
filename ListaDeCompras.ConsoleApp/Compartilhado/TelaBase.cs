18c46549eece46e0cd3c9b593e0b6eb6565a4e1d
Vinicius Poli <alemaumvp@hotmail.com>
2026-04-28 14:39:17 -0300

    Iniciado o repositório

---
+++ ListaDeCompras.ConsoleApp/Compartilhado/TelaBase.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ListaDeCompras.ConsoleApp.Compartilhado;
+
+public class TelaBase
+{
+
+}
---
+++ ListaDeCompras.ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ListaDeCompras.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
